aaf629a96fd7568f52ba8ad00d9bef6f0ae46608
Amir Akram <amir.akram@netsoltech.com>
2017-06-29 22:51:20 +0500

    -- Header Changes - -

--- Inbox/Inbox/MessageTableViewReceiverCell.swift
+++ Inbox/Inbox/MessageTableViewReceiverCell.swift
@@ -2,7 +2,7 @@
 //  MessageTableViewSenderCell.swift
 //  Inbox
 //
-//  Created by tahir on 20/06/2017.
+//  Created by Amir Akram on 21/06/2017.
 //  Copyright © 2017 Amir Akram. All rights reserved.
 //
 
